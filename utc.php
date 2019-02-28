246f920b57b531b32f976b5e50a9fe4fa8bd0e9d
VPrikalab <vprikalab@gmail.com>
2019-02-28 20:22:26 +0700

    1.0

---
+++ utc.php
@@ -0,0 +1,25 @@
+ <?php
+(float)$_POST['tel'];
+
+$data = array('phone' =>'79996479245');
+$filter = $data;
+$data_string = json_encode($filter);
+
+$url = 'http://geohelper.info/api/v1/phone-data?apiKey=k0OJnUIhJ3SR5DNWx3qRaSY5Wvdtl8JJ&locale%5Blang%5D=ru&filter%5Bphone%5D="79996479245"';
+
+$curl = curl_init($url);
+ 
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($data_string))                              
+);                                                                                                                   
+$result = curl_exec($curl);
+curl_close($curl); 
+
+$json_out = json_decode($result,true);
+file_put_contents('JSON.txt',$result); 
+echo $json_out["result"]["region"]["timezoneOffset"];
+?>
